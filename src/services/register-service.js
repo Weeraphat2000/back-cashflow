14fc645009d4581690cede55b69f24088eb2aa19
Weeraphat2000 <weeraphat.main@gmail.com>
2024-04-13 14:16:23 +0700

    finish

--- src/services/register-service.js
+++ src/services/register-service.js
@@ -6,3 +6,5 @@ exports.checkUserDuplicate = (body) =>
   prisma.user.findFirst({
     where: { OR: [{ email: body.email }, { mobile: body.mobile }] },
   });
+
+// exports.a = ()=>prisma.user.findMany({where:{id}})
